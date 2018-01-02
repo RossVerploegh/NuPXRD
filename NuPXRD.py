df96482e9f1e9e2aa15ed0345b2e33c3e110e60b
RossVerploegh <rverploe@alumni.nd.edu>
2018-01-02 05:53:27 -0800

    foray into classes

--- NuPXRD.py
+++ NuPXRD.py
@@ -1,25 +1,37 @@
 
 
-
-def read_pxrd(path):
-    """Parses a PXRD DAT file.
-
-    Args:
-        path - Path of PXRD DAT file, as a string.
-    Returns:
-        A nested list of the form [[x1, y1], [x2, y2], ...]
+class NuPXRD(object):
     """
-    data = []
 
-    with open(path) as in_file:
-        for row in in_file:
-            # Assignment - fill in the `data` variable
-            data=""
-    return data
+    Attributes:
+        Width:
+    """
 
-def write_csv():
-    with open('output.csv', 'w') as output_file:
-        output_file.write(your_csv_string)
+    def __init__(self, name, width, twotheta=[], intensity=[])
+        """ """"
+        self.width = width
+        self.twotheta = twotheta
+        self.intensity = intensity
+
+    def read_pxrd(path):
+        """Parses a PXRD DAT file.
+
+        Args:
+            path - Path of PXRD DAT file, as a string.
+        Returns:
+            A nested list of the form [[x1, y1], [x2, y2], ...]
+        """
+        data = []
+
+        with open(path) as in_file:
+            for row in in_file:
+                # Assignment - fill in the `data` variable
+                data=""
+        return data
+
+    def write_csv():
+        with open('output.csv', 'w') as output_file:
+            output_file.write(your_csv_string)
 
 
 
