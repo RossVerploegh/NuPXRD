e283735b90829dab4c7a3f5aefb9e782724f8b1a
Ross James Verploegh <rossjamesverploegh@Rosss-MBP.attlocal.net>
2018-01-14 22:40:47 -0600

    missing parenthesis

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
   author_email = 'ross@numat-tech.com',
   url = 'https://github.com/RossVerploegh/nupxrd', # use the URL to the github repo
   download_url = 'https://github.com/RossVerploegh/nupxrd/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['powder XRD', 'MOF', 'NuMat'], # arbitrary keywords
+  keywords = ['powderXRD', 'MOF', 'NuMat'], # arbitrary keywords
   classifiers = [],
+)
